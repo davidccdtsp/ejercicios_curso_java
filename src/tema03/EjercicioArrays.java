7316a125ba3e051c706f95f9ba53afba6c3d8f56
david.cc <david.cc@dataspartan.com>
2023-08-04 17:13:37 +0200

    wip: tema 3

---
+++ src/tema03/EjercicioArrays.java
@@ -0,0 +1,16 @@
+package tema03;
+
+public class EjercicioArrays {
+
+  public static void ejercicio01() {
+
+    // Crear el array numeros {1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365}.
+
+    // Crear el array palabras {“Apple”, “Google”, “Microsoft”, “Nvidia”, “Intel”, “AMD”, “TSMC”, “Samsung”, “Qualcom”, “Huawei”, “ASML”}.
+
+    // Imprimir por pantalla las posiciones 0, 4 y última de numeros.
+
+    // Imprimir por pantalla las posiciones 5, 6 y 10 de palabras.
+
+  }
+}
