b6983203af12d288fd242e42d8d4a23e3542a58e
david.cc <david.cc@dataspartan.com>
2023-08-07 09:47:31 +0200

    Tema 3

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import tema03.EjercicioArrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        EjercicioArrays.ejercicio01();
+        EjercicioArrays.ejercicio02();
+        EjercicioArrays.ejercicio03();
+        EjercicioArrays.ejercicio04();
+        EjercicioArrays.ejercicio05();
     }
 }
--- src/tema03/EjercicioArrays.java
+++ src/tema03/EjercicioArrays.java
@@ -1,16 +1,86 @@
 package tema03;
 
+import java.util.Arrays;
+
 public class EjercicioArrays {
 
   public static void ejercicio01() {
 
+    System.out.println("Tema 3: Arrays");
+    System.out.println("Ejercicio 1");
+
     // Crear el array numeros {1789, 2035, 1899, 1456, 2013, 1458, 2458, 1254, 1472, 2365}.
 
-    // Crear el array palabras {“Apple”, “Google”, “Microsoft”, “Nvidia”, “Intel”, “AMD”, “TSMC”, “Samsung”, “Qualcom”, “Huawei”, “ASML”}.
+    // Crear el array palabras {"Apple", "Google", "Microsoft", "Nvidia", "Intel", "AMD", "TSMC",
+    // "Samsung", "Qualcom", "Huawei", "ASML"};
 
-    // Imprimir por pantalla las posiciones 0, 4 y última de numeros.
+    // Mostrar por pantalla las posiciones 0, 4 y última de numeros.
 
-    // Imprimir por pantalla las posiciones 5, 6 y 10 de palabras.
+    // Mostrar por pantalla las posiciones 5, 6 y 10 de palabras.
+
 
   }
+
+  public static void ejercicio02() {
+
+    System.out.println("Ejercicio 2");
+
+    // Crear el array 3x3 matriz
+    // [1, 2, 3]
+    // [4, 5, 6]
+    // [7, 8, 9]
+    // int a[] = new int(5);
+
+    // Mostrar el contenido del array, usar: Arrays.toString(arrayQueQuieroMostarr)
+
+    // Mostrar las posiciones (0,1), (1,2) y (2,1)
+
+  }
+
+  public static void ejercicio03() {
+
+    System.out.println("Ejercicio 3");
+
+    // Declarar e inicializar los arrays para que su salida al mostrarlos por consola sea la
+    // indicada
+    Object a[] = null; // Modificar este codigo, NO usar Object
+    System.out.println(Arrays.toString(a)); // [true, true, false, true]
+    Object b[] = null;
+    System.out.println(Arrays.toString(b)); // [10, 20, 30]
+    Object c[] = null;
+    System.out.println(Arrays.toString(c)); // [g, e, e, k, s]
+    Object d[] = null;
+    System.out.println(Arrays.toString(d)); // [1.0, 2.0, 3.0, 4.0]
+    Object e[] = null;
+    System.out.println(Arrays.toString(e)); // [1, 2, 3, 4]
+
+  }
+
+  public static void ejercicio04() {
+    System.out.println("Ejercicio 4");
+
+    // Ordenar alfabéticamente y muestrael array palabras reado en el ejercicio 1.b y
+    // Ordenarlo alfabéticamente
+    // Buscar el valor “Qualcom”.
+    // Buscar el valor “Desconocido”.
+    // Buscar el valor “qualcom”.
+  }
+
+  public static void ejercicio05() {
+    System.out.println("Ejercicio 5");
+
+    // Usando el array numeros del ejercicio 1
+    // Cambiar todos sus valores a 0
+    // Cambia los valores entre las posiciones 4º y 8º a 1.
+    // *Usar una única sentencia para cada apartado.
+
+  }
+
+  public static void ejercicio06() {
+    System.out.println("Ejercicio 6");
+
+    // Copia el valor del array numeros dentro de un nuevo array llamado copia.
+
+  }
+
 }
